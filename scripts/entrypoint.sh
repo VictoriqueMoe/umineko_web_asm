719fbfafa12233c1b70ebb5c0b0429133f7843fa
LiviaMedeiros <livia@cirno.name>
2026-03-01 18:31:23 +0800

    make asset optimization nicer

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 /usr/local/bin/generate-manifest.sh
-/usr/local/bin/convert-assets.sh /usr/share/nginx/html/game /usr/share/nginx/html/cache/game &
+nice -n 19 ionice -c 3 -n 7 /usr/local/bin/convert-assets.sh /usr/share/nginx/html/game /usr/share/nginx/html/cache/game &
 exec nginx -g 'daemon off;'
